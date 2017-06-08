2018460edbb7f4854a7bb1cc34392527fd82ca19
Alex <alex@scotttonge.com>
2017-06-08 16:10:18 +0100

    third commit

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,26 +1,40 @@
-def prompt
-    puts "Please enter a number"
-    user_input = gets.chomp
+def multiple_3_5(a)
+  puts "Please enter a number"
+  inputs = gets.chomp.to_i
+  if a %3 == 0 && a %5 == 0
+    puts "Fizzbuzz"
+  elsif a %3 == 0
+    puts "Fizz"
+  elsif a %5 == 0
+    puts "Buzz"
+  else
+    puts a
+  end
 end
 
-def fizzbuzz
-prompt
-if user_input.class != Fixnum 
-    prompt
-    else 
-    
+multiple_3_5(2)
+
+
+#def fizzbuzz
+#  puts "Please enter a number"
+#  user_input = gets.chomp
+#if user_input.class != Integer
+#    puts "That's not a number, enter a number"
+#    else
+#    end
+
 # Yes => run if
 # No => "Please type a number"
- #    if 
-        
+ #    if
+
         #  (3*5)
         # elsif % 3 == 0
         # elsif % 5 == 0
-        # else return 
+        # else return
   #   end
-end
 
-fizzbuzz
+
+
 
 
 # >> 1.class #=> Fixnum
