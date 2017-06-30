be4a3b06ea54e70431a196593d9e591bea89bbf5
Gurjinder Massan <gurjinder.massan@gmail.com>
2017-06-30 11:41:40 +0100

    Works with multiples of 3 and 5, passes all tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(number)
     if number == 15
         return 'fizzbuzz'
-    elsif number == 5
+    elsif number % 5 == 0
         return 'buzz'
     elsif number % 3 == 0
         return 'fizz'
